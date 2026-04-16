53c947782cc548ab020c498e8d7dd9e2ea19fcbd
Leon-Degel-Koehn <leon.koehn2002@gmail.com>
2026-04-16 07:37:58 +0200

    Add left-right split
    This enables the later display of showing project details in the
    righthand block.

--- src/layout.rs
+++ src/layout.rs
@@ -1,5 +1,7 @@
 use ratatui::{
     Frame,
+    layout::{Constraint, Layout, Spacing},
+    symbols::merge::MergeStrategy,
     widgets::{Block, Borders, List, ListItem},
 };
 
@@ -18,10 +20,21 @@ pub fn draw(frame: &mut Frame, state: &mut AppState) {
         .block(
             Block::default()
                 .title(" Select an Item (j/k or Arrows) ")
-                .borders(Borders::ALL),
+                .borders(Borders::ALL)
+                .merge_borders(MergeStrategy::Exact),
         )
         .highlight_symbol(">> ")
         .repeat_highlight_symbol(true);
 
-    frame.render_stateful_widget(list, frame.area(), &mut state.project_list_state);
+    let [left, right] = Layout::horizontal([Constraint::Fill(1); 2])
+        .spacing(Spacing::Overlap(1))
+        .areas(frame.area());
+
+    frame.render_stateful_widget(list, left, &mut state.project_list_state);
+    frame.render_widget(
+        Block::bordered()
+            .title("Right")
+            .merge_borders(MergeStrategy::Exact),
+        right,
+    );
 }
