00ccdae5f7e062289a1299fae1aef72db28887e4
KeyVizsla <leon.koehn2002@gmail.com>
2026-05-01 20:13:48 +0200

    Delete old scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export FLOO_OUTPUT_FILE=$(mktemp)
-echo $FLOO_OUTPUT_FILE
-cargo run
-[ -s "$FLOO_OUTPUT_FILE" ] && eval "$(cat $FLOO_OUTPUT_FILE)"
-rm -f "$FLOO_OUTPUT_FILE"
-unset FLOO_OUTPUT_FILE
