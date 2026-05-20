18041d5255148a08bb49a436e5d2040caaa0d29e
KeyVizsla <leon.koehn2002@gmail.com>
2026-05-20 10:55:31 +0200

    Refactor edit notes

--- src/app.rs
+++ src/app.rs
@@ -42,37 +42,7 @@ pub struct App {
 }
 
 impl App {
-    fn output_path() -> PathBuf {
-        env::var("FLOO_OUTPUT_FILE")
-            .ok()
-            .map(PathBuf::from)
-            .unwrap()
-    }
-
-    fn init_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, io::Error> {
-        enable_raw_mode()?;
-        let mut stdout = io::stdout();
-        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-        let backend = CrosstermBackend::new(stdout);
-        Ok(Terminal::new(backend)?)
-    }
-
-    fn edit_notes(&mut self, notes: String) -> Result<String, io::Error> {
-        open_editor(notes, Some(".md".to_string()), &mut self.terminal)
-    }
-
-    fn edit_and_apply_template(
-        &mut self,
-        template: &PathBuf,
-        project: &Fireplace,
-    ) -> Result<(), io::Error> {
-        let file_contents = String::from_utf8(fs::read(template)?)
-            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid file contents"))?;
-        let edited = open_editor(file_contents, Some(".sh".to_string()), &mut self.terminal)?;
-        fs::write(project.get_directory().join(".floo"), edited)?;
-        Ok(())
-    }
-
+    /// Instantiate a new floo app instance.
     pub fn new() -> Result<Self, AppCreationError> {
         let state = AppState::init();
         let terminal = Self::init_terminal().map_err(|_| AppCreationError {})?;
@@ -83,22 +53,6 @@ impl App {
             terminal,
             tui,
         })
-    }
-
-    fn draw(&mut self) {
-        let _ = self
-            .terminal
-            .draw(|frame| self.tui.render(frame, frame.area()));
-    }
-
-    fn cleanup(&mut self) {
-        let _ = disable_raw_mode();
-        let _ = execute!(
-            self.terminal.backend_mut(),
-            LeaveAlternateScreen,
-            DisableMouseCapture
-        );
-        let _ = self.terminal.show_cursor();
     }
 
     /// Start the TUI with an open, prefilled fireplace
@@ -112,37 +66,14 @@ impl App {
         self.run()
     }
 
+    /// Run the app, this opens the TUI and handles key events and such.
     pub fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         loop {
             self.draw();
             if event::poll(std::time::Duration::from_millis(100))? {
                 let action = self.tui.handle_events(event::read().ok());
                 let tui_update = match action.clone() {
-                    Action::EditNotes(project) => {
-                        if let Ok(updated_notes) = self.edit_notes(project.notes.clone()) {
-                            let new_project = Fireplace::new(
-                                project.name.clone(),
-                                project.get_directory(),
-                                updated_notes.clone(),
-                                project.last_accessed.clone(),
-                            );
-                            match db_handler::change_notes(&project, &updated_notes) {
-                                Ok(_) => {
-                                    self.state.remove_project(&project);
-                                    self.state.replace_project(&project, new_project.clone());
-                                    Action::ReplaceFireplace {
-                                        old: project,
-                                        new: new_project,
-                                    }
-                                }
-                                Err(_) => Action::Error(FlooError::DbUpdateError(
-                                    "Failed to update notes".to_string(),
-                                )),
-                            }
-                        } else {
-                            Action::Noop
-                        }
-                    }
+                    Action::EditNotes(project) => self.edit_notes(project),
                     Action::Quit => {
                         self.cleanup();
                         return Ok(());
@@ -196,5 +127,73 @@ impl App {
                 self.tui.update(tui_update);
             }
         }
+    }
+
+    fn output_path() -> PathBuf {
+        env::var("FLOO_OUTPUT_FILE")
+            .ok()
+            .map(PathBuf::from)
+            .unwrap()
+    }
+
+    fn init_terminal() -> io::Result<Terminal<CrosstermBackend<Stdout>>> {
+        enable_raw_mode()?;
+        let mut stdout = io::stdout();
+        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+        let backend = CrosstermBackend::new(stdout);
+        Terminal::new(backend)
+    }
+
+    fn edit_notes(&mut self, project: Fireplace) -> Action {
+        if let Ok(updated_notes) = open_editor(
+            project.notes.clone(),
+            Some(".md".to_string()),
+            &mut self.terminal,
+        ) {
+            let mut new_project = project.clone();
+            new_project.notes = updated_notes.clone();
+            match db_handler::change_notes(&project, &updated_notes) {
+                Ok(_) => {
+                    self.state.replace_project(&project, new_project.clone());
+                    Action::ReplaceFireplace {
+                        old: project,
+                        new: new_project,
+                    }
+                }
+                Err(_) => Action::Error(FlooError::DbUpdateError(
+                    "Failed to update notes".to_string(),
+                )),
+            }
+        } else {
+            Action::Noop
+        }
+    }
+
+    fn edit_and_apply_template(
+        &mut self,
+        template: &PathBuf,
+        project: &Fireplace,
+    ) -> Result<(), io::Error> {
+        let file_contents = String::from_utf8(fs::read(template)?)
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid file contents"))?;
+        let edited = open_editor(file_contents, Some(".sh".to_string()), &mut self.terminal)?;
+        fs::write(project.get_directory().join(".floo"), edited)?;
+        Ok(())
+    }
+
+    fn draw(&mut self) {
+        let _ = self
+            .terminal
+            .draw(|frame| self.tui.render(frame, frame.area()));
+    }
+
+    fn cleanup(&mut self) {
+        let _ = disable_raw_mode();
+        let _ = execute!(
+            self.terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        );
+        let _ = self.terminal.show_cursor();
     }
 }
